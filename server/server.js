030325ef749dcc6fb5d22e00907263b36deac8bf
unknown <=>
2024-05-26 23:44:17 +0200

    new

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const cors = require('cors')
 
+// Configurez CORS pour autoriser l'origine spécifiée
+
+const corsOptions = {
+    origin: 'http://localhost:5173', // Remplacez par votre origine autorisée
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions))
 
 app.get("/api",(req,res) => {
     res.json({"users": ["userOne","userTwo"]})
